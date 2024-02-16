a258b22a2a29f83c3699d9e64146aa5791350bf1
surajpithva <surajpgithub@gmail.com>
2024-02-16 10:16:23 +0530

    modify bankist app

--- JavaScript Uptakeinfotech/udemy/bankingApp-List/script.js
+++ JavaScript Uptakeinfotech/udemy/bankingApp-List/script.js
@@ -3,18 +3,48 @@ const account1 = {
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2,
   pin: 1111,
+  movementsDates: [
+    "2019-11-01T13:15:33.035Z",
+    "2019-11-30T09:48:16.867Z",
+    "2019-12-25T06:04:23.907Z",
+    "2020-01-25T14:18:46.235Z",
+    "2020-02-05T16:33:06.386Z",
+    "2020-04-10T14:43:26.374Z",
+    "2020-06-25T18:49:59.371Z",
+    "2020-07-26T12:01:20.894Z",
+  ],
 };
 const account2 = {
   owner: "Parth Pithva",
   movements: [5200, 1500, -450, 1120, 1070, 1069, -4000, 920],
   interestRate: 1.5,
   pin: 2222,
+  movementsDates: [
+    "2019-11-18T21:31:17.178Z",
+    "2019-12-23T07:42:02.383Z",
+    "2020-01-28T09:15:04.904Z",
+    "2020-04-01T10:17:24.185Z",
+    "2020-05-08T14:11:59.604Z",
+    "2020-07-26T17:01:17.194Z",
+    "2020-07-28T23:36:17.929Z",
+    "2020-08-01T10:51:36.790Z",
+  ],
 };
 const account3 = {
   owner: "Darshan Pithva",
   movements: [1200, -500, 45, -20, 70, 16, -4000, 920],
   interestRate: 0.7,
   pin: 3333,
+  movementsDates: [
+    "2020-01-28T09:15:04.904Z",
+    "2020-04-01T10:17:24.185Z",
+    "2020-05-08T14:11:59.604Z",
+    "2019-11-30T09:48:16.867Z",
+    "2019-12-25T06:04:23.907Z",
+    "2020-01-25T14:18:46.235Z",
+    "2020-04-10T14:43:26.374Z",
+    "2020-06-25T18:49:59.371Z",
+  ],
 };
 const account4 = {
   owner: "Atul Pithva",
@@ -72,19 +102,29 @@ const inputClosePin = document.querySelector(".form__input--pin");
 // };
 // displayMoments(account1.movements);
 
-//display data with filter
+//////////////////////////////////////////display data with filter
 
 const displayMoments = function (movements, sort = false) {
   containerMovements.innerHTML = "";
   // sort method use here for sorting
   const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+
+  console.log(movements, "movements");
   movements.forEach((mov, i) => {
     const type = mov > 0 ? "deposit" : "withdrawal";
+    console.log(mov, "mveeee");
+    // const date = new Date(account1.movementsDates[i]);
+    // const day = `${now.getDate()}`.padStart(2, 0);
+    // const month = `${now.getMonth() + 1}`.padStart(2, 0);
+    // const year = now.getFullYear();
+
+    // const displayDate = `${day}/${month}/${year}`;
     const html = `
     <div class="movements__row">
         <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
+      <div class="movements__date">${accounts.movementsDates}</div>
         <div class="movements__value">${mov}</div>
     </div>
 `;
@@ -100,6 +140,7 @@ function filterWithdraw(movements) {
           <div class="movements__type movements__type--withdrawal movements__type--  ${index}" >  ${
       index + 1
     } WITHDRAW </div>
+    
           <div class="movements__value">${item}</div>
       </div>
     `;
@@ -119,6 +160,8 @@ function filterDeposits(movements) {
           <div class="movements__type movements__type--deposit movements__type--${index}">   ${
       index + 1
     } DEPOSIT </div>
+   
+
           <div class="movements__value">${item}</div>
       </div>
     `;
@@ -151,14 +194,14 @@ function val() {
   }
 }
 
-console.log(account1.movements);
+// console.log(account1.movements);
 
 /////////////////////////////////////calculate incomes , outcomes  and interest////////////////////////////////
 const calcDisplaySummery = function (acc) {
   const incomes = acc.movements
     .filter((mov) => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `${incomes}💶`;
+  labelSumIn.textContent = `${incomes.toFixed(2)}💶`;
 
   const outcomes = acc.movements
     .filter((mov) => mov < 0)
@@ -169,11 +212,11 @@ const calcDisplaySummery = function (acc) {
     .filter((mov) => mov > 0)
     .map((deposit) => (deposit * acc.interestRate) / 100)
     .filter((int, i, arr) => {
-      console.log(arr);
+      // console.log(arr);
       return int >= 1;
     })
     .reduce((acc, int) => acc + int, 0);
-  labelSumInterest.textContent = `${interst}`;
+  labelSumInterest.textContent = `${interst.toFixed(2)}`;
 };
 //calcDisplaySummery(account1.movements);
 ///////////////////////////////////computing User-Name//////////////////////////////////////////////////////////
@@ -201,12 +244,22 @@ const updateUI = function (acc) {
   calcDisplaySummery(acc);
 };
 
-console.log(accounts);
+// console.log(accounts);
+
+const now = new Date();
+const opetions = {
+  hour: "numeric",
+  minute: "numeric",
+  day: "numeric",
+  month: "long",
+  year: "numeric",
+};
+labelDate.textContent = new Intl.DateTimeFormat("en-US", opetions).format(now);
 
 //////////////////////////////////calcucate the balance///////////////////////////////////////////
 
 const calcDisplayBalance = (data) => {
-  console.log(data, "in calc");
+  // console.log(data, "in calc");
   const total_amt = data.reduce((acc, mov) => acc + mov, 0);
   currentAccount.balance = total_amt;
   labelBalance.textContent = `${total_amt} EUR`;
@@ -214,8 +267,11 @@ const calcDisplayBalance = (data) => {
 
 //calcDisplayBalance(account1.movements);
 
-///////////////////Event handler
-let currentAccount;
+///////////////////Event handler/////////////////////////////////////////
+///////Global variable
+let currentAccount, timer;
+////////////////////////////////////////////
+
 btnLogin.addEventListener("click", function (e) {
   //Prevent form from subbmiting
   e.preventDefault();
@@ -234,6 +290,8 @@ btnLogin.addEventListener("click", function (e) {
 
     // this below code for if you once log in your foucs is not input fidlds
     inputLoginPin.blur();
+    if (timer) clearInterval(timer);
+    timer = startLogOutTimer();
 
     updateUI(currentAccount);
   }
@@ -242,15 +300,18 @@ btnLogin.addEventListener("click", function (e) {
 // this part is request for loan
 btnLoan.addEventListener("click", function (e) {
   e.preventDefault(e);
-  const amount = Number(inputLoanAmount.value);
+  const amount = Math.floor(inputLoanAmount.value);
 
   if (
     amount > 0 &&
     currentAccount.movements.some((mov) => mov >= amount * 0.1)
   ) {
-    currentAccount.movements.push(amount);
-    //Update UI
-    updateUI(currentAccount);
+    //set time out function
+    setTimeout(function () {
+      currentAccount.movements.push(amount);
+      //Update UI
+      updateUI(currentAccount);
+    }, 3500);
   }
   inputLoanAmount.value = "";
 });
@@ -258,7 +319,7 @@ btnLoan.addEventListener("click", function (e) {
 /////////////////////////////////////////////
 btnTransfer.addEventListener("click", function (e) {
   e.preventDefault();
-  const amount = Number(inputTransferAmount.value);
+  const amount = Math.floor(inputTransferAmount.value);
   const receiverAcc = accounts.find(
     (acc) => acc.username === inputTransferTo.value
   );
@@ -267,12 +328,16 @@ btnTransfer.addEventListener("click", function (e) {
     amount > 0 &&
     currentAccount.balance >= amount &&
     receiverAcc?.username !== currentAccount.username;
-  console.log(checkIf);
+  // console.log(checkIf);
 
   if (checkIf) {
     currentAccount.movements.push(-amount);
     receiverAcc.movements.push(amount);
     updateUI(currentAccount);
+
+    //reset timer
+    clearInterval(timer);
+    //timer = startLogOutTimer();
   }
 });
 
@@ -318,29 +383,53 @@ const { deposits, withdraws } = accounts
       withdraws: 0,
     }
   );
-console.log(deposits, withdraws);
+// console.log(deposits, withdraws);
+
+//////////////////////setTimeout//////////////////////////////
+const startLogOut = function () {
+  const tick = function () {
+    const min = String(Math.trunc(time / 60)).padStart(2, 0);
+    const sec = String(Math.trunc(time % 60));
+    //in each call print the reming
+    labelTimer.textContent = `${min}:${sec}`;
+
+    if (time === 0) {
+      clearInterval(timer);
+      labelWelcome.textContent = `Log in to get started`;
+      containerApp.style.opacity = 0;
+    }
+    //decrese is
+    time--;
+  };
+  //set time out
+  let time = 10;
+  //call the timer every second
+  tick();
+  const timer = setInterval(tick, 1000);
+  return timer;
+};
 
 ////////////////////////////////////////preFixed Operator ////////////////////////////////////////
 //NOTE:- use ++ operator before console than you  js give original value
 let a = 10;
-console.log(a++);
-console.log(a++);
-console.log(a++);
+// console.log(a++);
+// console.log(a++);
+// console.log(a++);
 
 let b = 10;
-console.log(++b);
+// console.log(++b);
 
 /////////////////////////Example of convert string into array with function
-const convertTitleCase = function (title) {
-  const expections = ["a", "an", "the", "but", "or", "on", "in", "with"];
-  const tileCase = title
-    .toLowerCase()
-    .split(" ")
-    .map((word) =>
-      expections.includes(word) ? word : word[0].toUpperCase() + word.slice(1)
-    );
-  return tileCase;
-};
-console.log(convertTitleCase("this is nice title"));
-console.log(convertTitleCase("this is LONG title but  not long TOO"));
-console.log(convertTitleCase(" and here is another title with EXAMPLE"));
+// const convertTitleCase = function (title) {
+//   const expections = ["a", "an", "the", "but", "or", "on", "in", "with"];
+//   const tileCase = title
+//     .toLowerCase()
+//     .split(" ")
+//     .map((word) =>
+//       expections.includes(word) ? word : word[0].toUpperCase() + word.slice(1)
+//     );
+//   return tileCase;
+// };
+// console.log(convertTitleCase("this is nice title"));
+// console.log(convertTitleCase("this is LONG title but  not long TOO"));
+// console.log(convertTitleCase(" and here is another title with EXAMPLE"));
