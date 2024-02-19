5516e7437a64dbadc40707bbf2ed77a638ecbbe7
surajpithva <surajpgithub@gmail.com>
2024-02-19 18:59:31 +0530

    DOM-Traversing

--- JavaScript Uptakeinfotech/udemy/Advanced DOM and Events/script.js
+++ JavaScript Uptakeinfotech/udemy/Advanced DOM and Events/script.js
@@ -1,12 +1,17 @@
 "use strict";
 
-///////////////////////////////////////
+/////////////////////////////////////////////////////////////////
 // Modal window
-
+const btnScroll = document.querySelector(".btn--scroll-to");
+const section1 = document.querySelector("#section--1");
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".btn--close-modal");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabsContent = document.querySelectorAll(".operations__content");
+const nav = document.querySelector(".nav");
 
 const openModal = function (e) {
   e.preventDefault();
@@ -34,42 +39,120 @@ document.addEventListener("keydown", function (e) {
   }
 });
 
-const btnScroll = document.querySelector(".btn--scroll-to");
-const section1 = document.querySelector("#section--1");
-
 btnScroll.addEventListener("click", function (e) {
   const s1coords = section1.getBoundingClientRect();
-  console.log(s1coords);
-  console.log(e.target.getBoundingClientRect());
-  console.log("current scroll", window.pageXOffset, pageYOffset);
-  console.log(
-    "current height/width viewport",
-    document.documentElement.clientHeight,
-    document.documentElement.clientWidth
-  );
-  // old type scrolling
-  // window.scrollTo(
-  //   s1coords.left + window.pageXOffset,
-  //   s1coords.top + window.pageYOffset
-  // );
-  // window.scrollTo({
-  //   letf: s1coords.left + window.pageXOffset,
-  //   top: s1coords.top + window.pageYOffset,
-  //   behavior: "smooth",
-  // });
 
-  ///////////////////==================morden view of this scroll===========================//////////////////
-  section1.scrollIntoView({ behavior: "smooth" });
+  /////////////////////////////////////==================morden view of this scroll===========================//////////////////
+  section1.scrollIntoView({
+    behavior: "smooth",
+  });
 });
 
-const h1 = document.querySelector("h1");
+//////////////////////==Page navigation(slow down their position)==[1]/////////////////////
+// document.querySelectorAll(".nav__link").forEach(function (el) {
+//   el.addEventListener("click", function (e) {
+//     e.preventDefault();
+//     const id = this.getAttribute("href");
+//     console.log(id);
+//     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+//   });
+// });
 
-const alretH1 = function (e) {
-  alert("helllo");
-  h1.removeEventListener("mouseenter", alretH1);
+//////////////////////==Page navigation(slow down their position)==[2]/////////////////////
+
+document.querySelector(".nav__links").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  ////match the condition (how possible if we click on specific portion)
+  if (e.target.classList.contains("nav__link")) {
+    const id = e.target.getAttribute("href");
+    console.log(id);
+
+    document.querySelector(id).scrollIntoView({
+      behavior: "smooth",
+    });
+  }
+});
+
+/////////////////////////////////////////////////tabbed component///////////////////////////////////////////////////////
+
+// tabs.forEach((t) => t.addEventListener("click", () => console.log("hello")));
+
+tabsContainer.addEventListener("click", function (e) {
+  const clicked = e.target.closest(".operations__tab");
+
+  //Guard cluase
+  if (!clicked) return;
+  //remove active classes
+  tabs.forEach((t) => t.classList.remove("operations__tab--active"));
+  tabsContent.forEach((c) => c.classList.remove("operations__content--active"));
+
+  //activated tab
+  clicked.classList.add("operations__tab--active");
+
+  //active content area
+
+  document
+    .querySelector(
+      `.operations__content--$ {
+        clicked.dataset.tab
+      }
+
+      `
+    )
+    .classList.add("operations__content--active");
+});
+
+////////////////////////////////////////////////MADE FADE ANIMATION//////////////////////////////////////////////
+
+const handleHoverNavbar = function (e, opacity) {
+  if (e.target.classList.contains("nav__link")) {
+    const link = e.target;
+    const sibilings = link.closest(".nav").querySelectorAll(".nav__link");
+    const logo = link.closest(".nav").querySelector("img");
+
+    sibilings.forEach((el) => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
 };
-h1.addEventListener("mouseenter", alretH1);
 
-/////////////////////////=========================random color on child class================//////////////////
+//passing "" another argument
+nav.addEventListener("mouseover", handleHoverNavbar.bind(0.5));
 
-/////////////////////////=========================random color on parent class================//////////////////
+nav.addEventListener("mouseout", handleHoverNavbar.bind(1));
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// const h1 = document.querySelector("h1");
+
+// const alretH1 = function (e) {
+//   alert("helllo");
+//   h1.removeEventListener("mouseenter", alretH1);
+// };
+// h1.addEventListener("mouseenter", alretH1);
+
+/////////////////////////////////////GENERATE RANDOM COLOR ON NAV BAR WITH EXAMPLE OF BUBBLING AND CAPTURING/////////////////////////////
+// const randomInt = (min, max) =>
+//   Math.floor(Math.random() * (max - min + 1) + min);
+
+// const randomColor = () =>
+//   `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+// console.log(randomColor(0, 255));
+
+// document.querySelector(".nav__link").addEventListener("click", function (e) {
+//   this.style.backgroundColor = randomColor();
+// });
+
+// document.querySelector(".nav__links").addEventListener("click", function (e) {
+//   this.style.backgroundColor = randomColor();
+// });
+
+// document.querySelector("nav__link").addEventListener("click", function (e) {
+//   //this.style.backgroundColor = randomColor();
+// });
+
+//////////////////////////////example of parent and child elements siblings//////////////////////////////////////////////////////////
+// const h1 = document.querySelector("h1");
+// h1.closest(".header").style.background = "var(--gradient-secondary)";
+
+// console.log(h1.parentElement.children);
